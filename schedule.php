71bb8bc3b57ca78fe3a23881e49341f1c6a82cdb
Aryog <yogesh.772417@thc.tu.edu.np>
2023-01-20 08:39:34 +0545

    upto no Ui

---
+++ schedule.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mini event updates</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="images/fav.jpg">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+</head>
+
+<body style="background-color:#018a8a;">
+    <div class="whole">
+        <div class="container mt-4">
+            <h2>Live updates of mini events / workshop from Vertex Hacks!</h2>
+        </div>
+    </div>
+    <div class="container mt-4" id="events">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card">
+                    
+                    <img src="images/chess.jpg" class="card-img-top" alt="...">                   
+                    <div class="card-body">
+                        <h5 class="card-title">Chess Competition</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                        <button type="button" class="btn btn-danger" disabled>Registration closed</button>
+                        
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="images/chess.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Chess Competition</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                        
+                        <a href="https://vertexhacks.tech/" target="_blank"><button type="button" class="btn btn-success">Register Now</button></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <img src="images/chess.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Chess Competition</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.</p>
+                            <button type="button" class="btn btn-secondary" disabled>Registration not open</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
